ab2ed0f86a2afe9ce9b49ad761c108891702cb3f
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2019-12-11 01:59:56 +0200

    first init - creating react app

---
+++ react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello Team</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
