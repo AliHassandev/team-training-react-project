5e5bcbc84670cffe030726070d7b41f8c8c0016e
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2019-12-11 22:53:40 +0200

    explain this - const vs let - arrow function - Array.map - string template

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -5,3 +5,42 @@ const element = <h1>Hello Team</h1>;
 console.log(element);
 
 ReactDOM.render(element, document.getElementById("root"));
+
+const person = {
+    name: "Alosh",
+    walk(){
+        console.log(this);
+    },
+}
+
+person.walk();
+
+// using bind to assign this
+const person2 = person.walk.bind(person);
+person2();
+// =========
+// Arrow Function
+// =========
+
+// const square = function(number) {
+//     return number * number;
+// }
+const square = number => number * number;
+console.log(square(5));
+
+// using arrow function with filter method
+const jobs = [
+    { id: 1, isActive : true },
+    { id: 2, isActive : true },
+    { id: 3, isActive : false }
+]
+const activeJobs = jobs.filter(jobs => jobs.isActive);
+console.log(activeJobs);
+// arrow function don't rebind this keyword 
+
+// =========
+// Array.map
+// =========
+const colors = ["red", "green", "blue"];
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);
