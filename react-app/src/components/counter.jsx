327677d706dc806a518bf299653b424aae4bca4c
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2019-12-16 01:44:51 +0200

    using state with a component

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 
 //cc then tab is a shorthand for the below block
 class Counter extends Component {
-    state = {  }
+    state = {
+        count : 5
+      }
     render() { 
-        return <h1>Hello World</h1>;
+        return (<div>
+                    <h1>Hello World</h1>
+                    <span>{this.formatCount()}</span>
+                    <button>Increment</button>
+                </div>);
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
