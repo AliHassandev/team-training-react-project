93aa2d83faad28e432c427ac3309ce9ba5b66e8d
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2019-12-16 01:12:12 +0200

    installing Bootstrap and creating the first component

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//imrc then tab is a shorthand for the above line
+
+//cc then tab is a shorthand for the below block
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return <h1>Hello World</h1>;
+    }
+}
+ 
+export default Counter;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import Counter from './components/counter';
 //new practice - products Counter app 
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
