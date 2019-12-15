253fafad215e327186945ff7dceaf692d58e246f
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2019-12-16 00:54:00 +0200

    cleaning files to start another practice

---
+++ react-app/src/jsx-training/training.js
@@ -0,0 +1,97 @@
+// cleaning pages from previous practices and save them here as a reference 
+
+// import {Customer} from './jsx-training/customer';
+// import VipCustomer, {callBack} from './jsx-training/vipcustomer';
+
+const element = <h1>Hello Team</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
+
+const person = {
+    name: "Alosh",
+    walk(){
+        console.log(this);
+    },
+}
+
+person.walk();
+
+// using bind to assign this
+const person2 = person.walk.bind(person);
+person2();
+// =========
+// Arrow Function
+// =========
+
+// const square = function(number) {
+//     return number * number;
+// }
+const square = number => number * number;
+console.log(square(5));
+
+// using arrow function with filter method
+const jobs = [
+    { id: 1, isActive : true },
+    { id: 2, isActive : true },
+    { id: 3, isActive : false }
+];
+const activeJobs = jobs.filter(jobs => jobs.isActive);
+console.log(activeJobs);
+// arrow function don't rebind this keyword 
+
+// =========
+// Array.map
+// =========
+const colors = ["red", "green", "blue"];
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);
+
+// =========
+// Object Destructuring
+// =========
+const address = {
+    street : "",
+    city   : "",
+    country: ""
+};
+//const street    = address.street;
+//const city      = address.city;
+//const country   = address.country;
+
+const {street, city, country} = address;
+// or we can use an alias to use another name to the variable 
+const {street: st} = address; // the variable name now is st
+
+// =========
+// Spread Operator
+// =========
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+//const combined = first.concat(second);
+// const combined = [...first, ...second];
+// const combined = [...first,'a', ...second, 'b']; we can use it to add values in between
+// and we can use it to clone and aaray 
+const cloned = [...first] // to clone the first array
+// we can also apply spread operator on objects 
+
+const firstObject = {name : "Alosh"};
+const secondObject = {job: "Developer"};
+
+const combinedObject = {...firstObject, ...secondObject, location: 'LB'};
+
+const clonedObject = {...firstObject};
+console.log(combinedObject);
+
+
+// =========
+// Classes
+// =========
+// the benefits of using Classes
+const customer01 = new Customer("Alosh");
+console.log(customer01.name);
+
+// Inheritance
+const majd = new VipCustomer("majd", "fintech");
+majd.bookAppointment();
