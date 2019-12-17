48955e09b5f250021884d7b3c8ec61f21f469f1c
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2019-12-17 22:44:24 +0200

    setting attributes - styles - calsses dynamically

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -4,20 +4,33 @@ import React, { Component } from 'react';
 //cc then tab is a shorthand for the below block
 class Counter extends Component {
     state = {
-        count : 5
-      }
+        count : 0
+      };
+
+    styles = {
+        fontSize: 22,
+        color: '#333',
+        fontWeight: 'bold'
+    }; 
     render() { 
         return (<div>
-                    <h1>Hello World</h1>
-                    <span>{this.formatCount()}</span>
-                    <button>Increment</button>
+                    <h1 style={{ fontSize:33, fontWeight: 'bold', color:'red' }}>Hello World</h1>
+                    <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                    <button className="btn btn-secondary">Increment</button>
                 </div>);
+    }
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
+
 }
  
 export default Counter;
