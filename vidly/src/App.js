069ca62570b9e69ef5d904404d60087aa3f4a328
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-01-02 00:32:47 +0200

    setting up Vidly app

---
+++ vidly/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main id="root" role="main" class="container">
+
+      <div class="starter-template">
+        <h1>Hello World</h1>
+      </div>
+
+    </main>
+      );
+}
+
+export default App;
