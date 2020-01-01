42c1a342960d47492474e9dd9197d494045eec60
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-01-01 22:59:36 +0200

    binding event handler with arrow func & passing event arguments

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -8,10 +8,10 @@ class Counter extends Component {
         tags: ["tag1", "tag2", "tag3"]
       };
 
-      constructor() {
-          super();
-          this.handleIncrement = this.handleIncrement.bind(this);
-      }
+    //   constructor() {
+    //       super();
+    //       this.handleIncrement = this.handleIncrement.bind(this);
+    //   }
 
     styles = {
         fontSize: 22,
@@ -22,7 +22,8 @@ class Counter extends Component {
         return (<div>
                     <h1 style={{ fontSize:33, fontWeight: 'bold', color:'red' }}>Hello World</h1>
                     <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                    <button onClick={this.handleIncrement}
+                    <button 
+                    onClick={ () => this.handleIncrement({id : 1}) }
                     className="btn btn-secondary">Increment</button>
 
             { (this.state.tags.length === 0 && <p>Please add tags</p>)}
@@ -31,27 +32,28 @@ class Counter extends Component {
                 </div>);
     }
 
-    handleIncrement () {
+    handleIncrement = product => {
+        console.log(product);
         //change the state 
         this.setState( { count: this.state.count + 1 } );
-    }
+    };
 
     renderTagsList(){
         if(this.state.tags.length === 0) return <p>There is no tags!</p>;
 
         return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }</ul>;
-    }
+    };
 
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
-    }
+    };
 
     formatCount(){
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
-    }
+    };
 
 }
  
