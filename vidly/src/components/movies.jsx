c611ef0749fe7d2039636b59b18ea2b2600bbc9e
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-01-30 23:37:26 +0200

    Vidly: extracting moviesTable to sfc

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { getMovie, getMovies } from '../services/fakeMovieService';
-import { getGenres } from '../services/fakeGenreService';
-import Like from './common/like';
+import MoviesTable from './moviesTable';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
 // import _ from 'lodash';
+import { getMovie, getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
+
 import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
@@ -73,35 +74,12 @@ class Movies extends Component {
 
                 <div className="col">
                     <p>Showing {filtered.length} movies in the database.</p>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { movies.map(movie => (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <Like 
-                                    liked={movie.liked} 
-                                    onClick={() => this.handleLike(movie)} 
-                                    />
-                                </td>
-                                <td><button onClick={ () => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
+
+                    <MoviesTable
+                    movies={movies}
+                    onLike={this.handleLike}
+                    onDelete={this.handleDelete} 
+                    />
 
                     <Pagination 
                     itemsCount={ filtered.length }
