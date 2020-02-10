34268df8f351420460bd5395fee032a6c2b83cf1
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-02-10 22:53:13 +0200

    Vidly filter: extracting method - destructuring arguments

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -52,25 +52,33 @@ class Movies extends Component {
         this.setState( {sortColumn} )
     };
 
-    render() {  
+    getPagedData = () => {
 
-        const {length: count} = this.state.movies;
         const {currentPage, 
             pageSize, 
             sortColumn,
             selectedGenre, 
             movies: allMovies} = this.state;
+            
+        const filtered = selectedGenre && selectedGenre._id
+        ?allMovies.filter(m => m.genre._id === selectedGenre._id)
+        :allMovies;
+
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const movies = paginate(sorted, currentPage, pageSize);
+
+        return {totalCount: filtered.length, data: movies};
+    };
+
+    render() {  
+
+        const {length: count} = this.state.movies;
+        const {currentPage, pageSize, sortColumn} = this.state;
         if(count === 0)
             return <p>There is no movies in the database.</p>;
 
-            const filtered = selectedGenre && selectedGenre._id
-            ?allMovies.filter(m => m.genre._id === selectedGenre._id)
-            :allMovies;
-
-            const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
-
-            const movies = paginate(sorted, currentPage, pageSize);
-
+        const {totalCount, data: movies} = this.getPagedData();
         return (
             <div className='row'>
                 <div className="col-3">
@@ -82,7 +90,7 @@ class Movies extends Component {
                 </div>
 
                 <div className="col">
-                    <p>Showing {filtered.length} movies in the database.</p>
+                    <p>Showing {totalCount} movies in the database.</p>
 
                     <MoviesTable
                     movies={movies}
@@ -93,7 +101,7 @@ class Movies extends Component {
                     />
 
                     <Pagination 
-                    itemsCount={ filtered.length }
+                    itemsCount={ totalCount }
                     pageSize={ pageSize }
                     currentPage={ currentPage }
                     onPageChange={this.handlePageChange} 
