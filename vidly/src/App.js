77eb54d780e55cc6bdb8668809bd15ad7aed16a2
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-02-16 23:59:45 +0200

    Vidly app - Forms: building bootstrap form - Handling submit

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -7,6 +7,7 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
     <NavBar />
     <main id="root" role="main" className="container">
       <Switch>
+        <Route path="/login" component={LoginForm} />
         <Route path="/movies/:id" component={MovieForm} />
         <Route path="/movies" component={Movies} />
         <Route path="/customers" component={Customers} />
