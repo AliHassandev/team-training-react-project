9463619171ef9af5e2cddde2cb45785ee0033ad8
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-02-18 00:06:56 +0200

    Vidly app: Handling Mlutiple inputs

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -3,34 +3,61 @@ import React, { Component } from 'react';
 class LoginForm extends Component {
 
     //dont over use Ref, only when it's necessary 
-    username = React.createRef();
+    // username = React.createRef();
 
-    componentDidMount() {
-        this.username.current.focus();
-    };
+    // componentDidMount() {
+    //     this.username.current.focus();
+    // };
+    state = {
+        account: {
+            username:'',
+            password:''
+        }
+    }
 
     handleSubmit = e => {
         e.preventDefault();
         // call the server 
         console.log("Submitted");
-        const username = this.username.current.val();
+        // const username = this.username.current.val();
     };
 
+    handleChaneg = ( { currentTarget: input } ) => {
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState( {account} );
+    };
+
+    
     render() { 
+        const { account } = this.state;
+
         return ( <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
                     <input 
-                    ref={this.username}
+                    autoFocus
+                    // ref={this.username}
+                    value={account.username}
+                    onChange={this.handleChaneg}
+                    name="username"
                     id="username" 
                     type="text" 
-                    className="form-control"/>
+                    className="form-control"
+                    />
                     </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="text" className="form-control"/>
+                    <input 
+                    value={account.password}
+                    onChange={this.handleChaneg}
+                    name="password"
+                    id="password" 
+                    type="text" 
+                    className="form-control"
+                    />
                     </div>
                     <button className="btn btn-primary">Login</button>
             </form>
