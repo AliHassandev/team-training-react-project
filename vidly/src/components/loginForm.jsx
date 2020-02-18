0b118e533047b2d86e58aefcc98511f7eb0334d6
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-02-18 23:48:30 +0200

    Vidly app: Displying validation errors

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -30,7 +30,8 @@ class LoginForm extends Component {
 
         const errors = this.validate();
         console.log(errors);
-        this.setState( { errors } );
+        //errors should be always an object, it shouldn't be null
+        this.setState( { errors: errors || {} } );
         if(errors) return;
         // call the server 
         console.log("Submitted");
@@ -44,7 +45,7 @@ class LoginForm extends Component {
 
     
     render() { 
-        const { account } = this.state;
+        const { account, errors } = this.state;
 
         return ( <div>
             <h1>Login</h1>
@@ -54,12 +55,14 @@ class LoginForm extends Component {
                     value={account.username}
                     label="Username"
                     onChange={this.handleChaneg}
+                    error={errors.username}
                 />
                 <Input 
                     name="password"
                     value={account.password}
                     label="Password"
                     onChange={this.handleChaneg}
+                    error={errors.password}
                 />
                 <button className="btn btn-primary">Login</button>
             </form>
