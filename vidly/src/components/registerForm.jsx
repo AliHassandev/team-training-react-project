9935099d1cb17a10350aaa7784b6c0874fda0749
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-03-21 23:13:12 +0200

    Hnadling registration errors

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
+import * as userService from '../services/userService'
 
 class RegisterForm extends Form {
     state = { 
@@ -24,9 +25,16 @@ class RegisterForm extends Form {
             .label("Name")
     };
 
-    doSubmit = () => {
-        // call the server 
-        console.log("Submitted");
+    doSubmit = async () => {
+        try {
+            await userService.register(this.state.data);
+        } catch (ex){
+            if(ex.response && ex.response.status === 400) {
+                const errors = {...this.state.errors};
+                errors.username = ex.response.data;
+                this.setState( {errors} );
+            }
+        }
     };
 
 
