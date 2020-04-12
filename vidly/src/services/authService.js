4ce284f90bb2f1054983be0c269c3a5e88ea74df
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-04-13 00:46:43 +0300

    Vidly and api: calling protected api endpoint - fixing Bi-directional dependencies

--- vidly/src/services/authService.js
+++ vidly/src/services/authService.js
@@ -6,6 +6,8 @@ import { apiUrl } from '../config.json';
 const apiEndpoint = apiUrl + "/auth";
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login(email, password){
     const { data: jwt } = await http.post(apiEndpoint, { email, password });
     localStorage.setItem(tokenKey, jwt);
@@ -29,9 +31,14 @@ export function getCurrentUser(){
           }
 }
 
+export function getJwt(){
+    return localStorage.getItem(tokenKey);
+}
+
 export default {
     login,
     loginWithJwt,
     logout,
-    getCurrentUser
+    getCurrentUser,
+    getJwt
 };
--- vidly/src/services/httpService.js
+++ vidly/src/services/httpService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import logger from './logService'
+import logger from './logService';
 import { toast } from 'react-toastify';
+
 
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && 
@@ -15,9 +16,15 @@ axios.interceptors.response.use(null, error => {
     return Promise.reject(error);
   });
 
+  function setJwt(jwt){
+    axios.defaults.headers.common['x-auth-token'] = jwt;
+
+  }
+
   export default {
       get: axios.get,
       post: axios.post,
       put: axios.put,
-      delete: axios.delete
+      delete: axios.delete,
+      setJwt
   }
