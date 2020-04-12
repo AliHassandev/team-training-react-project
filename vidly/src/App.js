3f90a4478cc05b0c9950cd46d145f680ce97daf7
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-04-13 00:55:35 +0300

    Vidly app: showing or hiding elements based on the user

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -38,7 +38,8 @@ import JwtDecode from 'jwt-decode';
           <Route path="/login" component={LoginForm} />
           <Route path="/logout" component={Logout} />
           <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
+          <Route path="/movies" 
+                  render={props => <Movies {...props} user={this.state.user} />} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
           <Route path="/not-found" component={NotFound} />
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -104,6 +104,8 @@ class Movies extends Component {
 
         const {length: count} = this.state.movies;
         const {currentPage, pageSize, sortColumn, searchQuery} = this.state;
+        const { user } = this.props;
+
         if(count === 0)
             return <p>There is no movies in the database.</p>;
 
@@ -119,11 +121,12 @@ class Movies extends Component {
                 </div>
 
                 <div className="col">
-                    <Link
+                    {user && <Link
                     to="/movies/new"
                     className="btn btn-primary"
                     style={{marginBottom: 20}}
-                    >New Movie</Link>
+                    >New Movie</Link>}
+                    
                     <p>Showing {totalCount} movies in the database.</p>
 
                     <SearchBox value={searchQuery} onChange={this.handleSearch} />
